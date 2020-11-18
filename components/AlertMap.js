c43162ff97bb9f10bd23215d581ece302305e8b3
Kristofor Carle <kris@maphubs.com>
2020-11-18 14:56:28 -0500

    update to latest detection data

--- components/AlertMap.js
+++ components/AlertMap.js
@@ -14,7 +14,7 @@ const monthNamesEn = ['January', 'February', 'March', 'April', 'May', 'June',
 ]
 
 const minMonth = 1
-const maxMonth = 7
+const maxMonth = 9
 
 export default function AlertMap ({t, mapLayers}: {t: Function, mapLayers: Array<Object>}) {
   const [glStyle, setGlStyle] = useState()
